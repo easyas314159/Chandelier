2a2998a201e0b31241d916279b280ce6bbf71b0f
root <root@chandelier-teacups.(none)>
2013-09-23 06:38:25 +0000

    Started adding webservice code to daemon

--- chandelier.py
+++ chandelier.py
@@ -5,6 +5,8 @@ import signal
 import RPi.GPIO as GPIO
 from daemon import runner
 
+frequency = 50
+
 class App():
 	def __init__(self):
 		self.stdin_path = '/dev/null'
@@ -14,18 +16,27 @@ class App():
 		self.pidfile_timeout = 5
 
 	def run(self):
+		GPIO.setwarnings(False)
+
 		GPIO.setmode(GPIO.BOARD)
 		GPIO.setup(11, GPIO.OUT)
 		GPIO.setup(13, GPIO.OUT)
 		GPIO.setup(15, GPIO.OUT)
 
-		signal.signal(signal.SIGTERM, self.shutdown)
+		self.pr = GPIO.PWM(13, frequency)
+		self.pg = GPIO.PWM(15, frequency)
+		self.pb = GPIO.PWM(11, frequency)
+
+		self.pr.start(100.0)
+		self.pg.start(100.0)
+		self.pb.start(100.0)
 
 		while True:
 			pass
 
-	def shutdown():
+	def shutdown(signal, frame):
 		GPIO.cleanup()
+		sys.exit(0)
 
 daemon_runner = runner.DaemonRunner(App())
 #This ensures that the logger file handle does not get closed during daemonization
