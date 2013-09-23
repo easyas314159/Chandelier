d06cddd8c4674aa5dcf84c0d0281f0b27d987fcb
Kevin Loney <kevin.loney@gmail.com>
2013-09-22 23:51:56 -0600

    Added daemon script

---
+++ chandelier.sh
@@ -0,0 +1,43 @@
+#! /bin/bash
+# /etc/init.d/chandelier
+#
+### BEGIN INIT INFO
+# Provides: chandelier
+# Required-Start: 
+# Should-Start: 
+# Required-Stop: 
+# Should-Stop:
+# Default-Start:  3 5
+# Default-Stop:   0 1 2 6
+# Short-Description: Chandelier daemon process
+# Description: 
+### END INIT INFO
+
+# Activate the python virtual environment
+    . /path_to_virtualenv/activate
+
+path=/usr/share/chandelier/daemon.py
+
+case "$1" in
+  start)
+    echo "Starting chandelier client"
+    # Start the daemon 
+    python "$path" start
+    ;;
+  stop)
+    echo "Stopping chandelier client"
+    # Stop the daemon
+    python "$path" stop
+    ;;
+  restart)
+    echo "Restarting chandelier client"
+    python "$path" restart
+    ;;
+  *)
+    # Refuse to do other stuff
+    echo "Usage: /etc/init.d/chandelier {start|stop|restart}"
+    exit 1
+    ;;
+esac
+
+exit 0
